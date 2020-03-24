d6b396ac133cb34b1abd5a2e1c413f8a6ad54e36
Tuomo Karki <tuomo.karki@hackney.gov.uk>
2020-03-24 12:10:17 +0000

    add root cert

--- mat-reporting-api/V1/Infrastructure/ProcessDbContext.cs
+++ mat-reporting-api/V1/Infrastructure/ProcessDbContext.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace MaTReportingAPI.V1.Infrastructure
 {
@@ -12,6 +15,28 @@ namespace MaTReportingAPI.V1.Infrastructure
 
         public ProcessDbContext(IOptions<ConnectionSettings> appSettings)
         {
+            string pathToCAFile = "/ssl/rds-ca-2019-root.pem";
+
+            X509Store localTrustStore = new X509Store(StoreName.Root);
+            string caContentString = System.IO.File.ReadAllText(pathToCAFile);
+
+            X509Certificate2 caCert = new X509Certificate2(Encoding.ASCII.GetBytes(caContentString));
+
+            try
+            {
+                localTrustStore.Open(OpenFlags.ReadWrite);
+                localTrustStore.Add(caCert);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Root certificate import failed: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                localTrustStore.Close();
+            }
+
             mongoClient = new MongoClient(new MongoUrl(appSettings.Value.ConnectionString));
             //create a new blank database if database does not exist, otherwise get existing database
             mongoDatabase = mongoClient.GetDatabase(appSettings.Value.Database);
